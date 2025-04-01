798c7c0a509f4b030f3a433a892752cbbc1995c2
Nicolas Pereira <nicolas.pp333@gmail.com>
2025-03-31 21:32:31 -0300

    Practico 1

--- practico1/main.cpp
+++ practico1/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_opengl.h>
+#include <SDL2/SDL_image.h>
 #include <GL/glut.h>
 
 using namespace std;
@@ -7,12 +9,58 @@ using namespace std;
 // Window dimensions
 const int WIDTH = 640;
 const int HEIGHT = 480;
+GLuint texture;
+
+GLuint LoadTexture(const char *filename)
+{
+    SDL_Surface *image = IMG_Load(filename);
+    if (!image)
+    {
+        cerr << "Error cargando imagen: " << IMG_GetError() << endl;
+        return 0;
+    }
+
+    GLuint textureID;
+    glGenTextures(1, &textureID);
+    glBindTexture(GL_TEXTURE_2D, textureID);
+
+    // Configurar formato de la imagen
+    GLenum format = (image->format->BytesPerPixel == 4) ? GL_RGBA : GL_RGB;
+
+    glTexImage2D(GL_TEXTURE_2D, 0, format, image->w, image->h, 0, format, GL_UNSIGNED_BYTE, image->pixels);
+
+    // Configurar los parámetros de la textura
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    SDL_FreeSurface(image); // Liberar memoria
+    return textureID;
+}
+
+void ResetOpenGL(int width, int height)
+{
+    glViewport(0, 0, width, height);
+    glMatrixMode(GL_PROJECTION);
+    float color = 0.3f;
+    glClearColor(color, color, color, 1.0f);
+    glLoadIdentity();
+    gluPerspective(45.0f, (float)width / (float)height, 0.1f, 100.0f);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+}
 
 int main(int argc, char *argv[])
 {
     if (SDL_Init(SDL_INIT_VIDEO) != 0)
     {
         cerr << "No se pudo iniciar SDL: " << SDL_GetError() << endl;
+        return 1;
+    }
+
+    if (IMG_Init(IMG_INIT_PNG) == 0)
+    {
+        cerr << "No se pudo iniciar SDL_image: " << IMG_GetError() << endl;
+        SDL_Quit();
         return 1;
     }
 
@@ -40,15 +88,18 @@ int main(int argc, char *argv[])
 
     glMatrixMode(GL_PROJECTION);
 
-    float color = 0.0f;
+    float color = 0.3f;
     glClearColor(color, color, color, 1.0f);
 
-    gluPerspective(45.0f, (float)WIDTH / (float)HEIGHT, 0.1f, 100.0f);
+    gluPerspective(30.0f, (float)WIDTH / (float)HEIGHT, 0.1f, 100.0f);
     glEnable(GL_DEPTH_TEST);
     glMatrixMode(GL_MODELVIEW);
 
+    texture = LoadTexture("opengl.png");
+
     bool fin = false;
     bool rotate = false;
+    bool fullscreen = false;
 
     SDL_Event evento;
 
@@ -59,6 +110,7 @@ int main(int argc, char *argv[])
     z = 5.0f;
 
     float degrees = 0.0f;
+    float scale = 1.0f;
 
     do
     {
@@ -73,7 +125,33 @@ int main(int argc, char *argv[])
         glRotatef(degrees, 0.0, 1.0, 0.0);
 
         glBegin(GL_TRIANGLES);
+        glColor3f(0.0, 1.0, 0.0);
+        glVertex3f(-1.5, 1., -6.);
+        glVertex3f(-2.5, -1., -6.);
+        glVertex3f(-0.5, -1., -6.);
+        glEnd();
+
+        glEnable(GL_TEXTURE_2D);
+        glBindTexture(GL_TEXTURE_2D, texture);
+
+        glBegin(GL_QUADS);
+
         glColor3f(1.0, 1.0, 1.0);
+        glTexCoord2f(501. / 512., 263. / 512.);
+        glVertex3f(scale * 452 / 512, -scale * 212 / 512, 0.05);
+        glTexCoord2f(49. / 512., 263. / 512.);
+        glVertex3f(-scale * 452 / 512, -scale * 212 / 512, 0.05);
+        glTexCoord2f(49. / 512., 51. / 512.);
+        glVertex3f(-scale * 452 / 512, scale * 212 / 512, 0.05);
+        glTexCoord2f(501. / 512., 51. / 512.);
+        glVertex3f(scale * 452 / 512, scale * 212 / 512, 0.05);
+
+        glEnd();
+
+        glDisable(GL_TEXTURE_2D);
+
+        glBegin(GL_TRIANGLES);
+        glColor3f(0.0, 1.0, 0.0);
         glVertex3f(1., -1., 0.);
         glColor3f(1.0, 0.0, 0.0);
         glVertex3f(-1., -1., 0.);
@@ -104,6 +182,37 @@ int main(int argc, char *argv[])
                     fin = true;
                     break;
                 case SDLK_RIGHT:
+                    rotate = !rotate;
+                    break;
+                case SDLK_UP:
+                    scale += 0.1f;
+                    break;
+                case SDLK_DOWN:
+                    scale -= 0.1f;
+                    break;
+                case SDLK_F10:
+                    fullscreen = !fullscreen;
+
+                    SDL_GL_DeleteContext(glContext);
+                    SDL_SetWindowFullscreen(window, fullscreen ? SDL_WINDOW_FULLSCREEN : 0);
+
+                    if (!fullscreen)
+                    {
+                        SDL_SetWindowSize(window, WIDTH, HEIGHT);
+                        SDL_SetWindowPosition(window, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED);
+                    }
+
+                    glContext = SDL_GL_CreateContext(window);
+                    if (!glContext)
+                    {
+                        cerr << "Error al recrear OpenGL context: " << SDL_GetError() << endl;
+                        fin = true;
+                        break;
+                    }
+
+                    int w, h;
+                    SDL_GetWindowSize(window, &w, &h);
+                    ResetOpenGL(w, h);
                     break;
                 }
             }
@@ -112,8 +221,10 @@ int main(int argc, char *argv[])
         SDL_GL_SwapWindow(window);
     } while (!fin);
 
+    glDeleteTextures(1, &texture);
     SDL_GL_DeleteContext(glContext);
     SDL_DestroyWindow(window);
     SDL_Quit();
+    IMG_Quit();
     return 0;
 }
